fece239df10fcc091273d6723ab9702a506c32a1
M4n0x <M4n0x@users.noreply.github.com>
2020-10-13 21:43:14 +0200

    added user table and group_user lookup table

---
+++ database/migrations/2020_10_13_193221_create_group_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_user', function (Blueprint $table) {
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('groupe_id');
+            $table->timestamps();
+            $table->boolean('isApprouved')->nullable(); // null -> pending, false -> refused, true accepted
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_user');
+    }
+}
