beb7d3e28e66025ad4bb3dce9d9d8609aa905c2f
M4n0x <M4n0x@users.noreply.github.com>
2020-10-13 23:46:36 +0200

    Added Group and Subject models

---
+++ database/migrations/2020_10_13_201639_create_groups_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('picture');
+            $table->timestamps();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
--- database/migrations/2020_10_13_204631_create_subjects_table.php
+++ database/migrations/2020_10_13_204631_create_subjects_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateGroupUserTable extends Migration
+class CreateSubjectsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,17 +13,15 @@ class CreateGroupUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('group_user', function (Blueprint $table) {
-            $table->index('user_id');
-            $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 150);
+            $table->smallInteger('color')->default(0);
+            
             $table->unsignedInteger('groupe_id');
             $table->index('group_id');
             $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
-            
-            $table->timestamps();
-            $table->boolean('isApprouved')->nullable(); // null -> pending, false -> refused, true accepted
         });
     }
 
@@ -34,6 +32,6 @@ class CreateGroupUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('group_user');
+        Schema::dropIfExists('subjects');
     }
 }
