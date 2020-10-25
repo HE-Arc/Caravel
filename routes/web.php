449663346a8fa7a5ff815e7b548d28c4ae69c193
M4n0x <M4n0x@users.noreply.github.com>
2020-10-25 16:01:58 +0100

    init argon theme

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,40 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('groups', TaskController::class);
+Route::resource('groups.tasks', TaskController::class);
+Route::resource('groups.subjects', SubjectController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
+	 Route::get('map', function () {return view('pages.maps');})->name('map');
+	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
+	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+});
+
+Auth::routes();
+
+Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+	Route::get('upgrade', function () {return view('pages.upgrade');})->name('upgrade'); 
+	 Route::get('map', function () {return view('pages.maps');})->name('map');
+	 Route::get('icons', function () {return view('pages.icons');})->name('icons'); 
+	 Route::get('table-list', function () {return view('pages.tables');})->name('table');
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
+});
+
