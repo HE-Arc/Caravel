f092b6dc750f5a77378c49718030a0afca7bb1d0
M4n0x <M4n0x@users.noreply.github.com>
2020-10-27 14:36:10 +0100

    missing var

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ $title }}</title>
+        <title>{{ $title ?? '' }}</title>
         <!-- Favicon -->
         <link href="{{ asset('argon') }}/img/brand/favicon.png" rel="icon" type="image/png">
         <!-- Fonts -->
