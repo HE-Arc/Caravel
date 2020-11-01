a5c5f7f938dc6ec38f66c9913a99e1a6059c2746
Monnet Joris <joris.monnet@he-arc.ch>
2020-11-01 17:09:36 +0100

    remove useless parts of the settings

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     @include('users.partials.header', [
         'title' => __('Hello') . ' '. auth()->user()->name,
-        'description' => __('This is your profile page. You can see the progress you\'ve made with your work and manage your settings'),
+        'description' => __('This is your profile page where you can manage your settings'),
         'class' => 'col-lg-7'
     ])   
 
@@ -21,23 +21,17 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                        <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-sm btn-info mr-4">{{ __('Connect') }}</a>
-                            <a href="#" class="btn btn-sm btn-default float-right">{{ __('Message') }}</a>
-                        </div>
-                    </div>
-                    <div class="card-body pt-0 pt-md-4">
+                    <div class="card-body pt-0 pt-md-6">
                         <div class="row">
                             <div class="col">
                                 <div class="card-profile-stats d-flex justify-content-center mt-md-5">
                                     <div>
                                         <span class="heading">22</span>
-                                        <span class="description">{{ __('Friends') }}</span>
+                                    <span class="description">{{ __('Tasks') }}</span>
                                     </div>
                                     <div>
                                         <span class="heading">10</span>
-                                        <span class="description">{{ __('Photos') }}</span>
+                                        <span class="description">{{ __('Classes') }}</span>
                                     </div>
                                     <div>
                                         <span class="heading">89</span>
@@ -48,20 +42,8 @@
                         </div>
                         <div class="text-center">
                             <h3>
-                                {{ auth()->user()->name }}<span class="font-weight-light">, 27</span>
+                                {{ auth()->user()->name }}
                             </h3>
-                            <div class="h5 font-weight-300">
-                                <i class="ni location_pin mr-2"></i>{{ __('Bucharest, Romania') }}
-                            </div>
-                            <div class="h5 mt-4">
-                                <i class="ni business_briefcase-24 mr-2"></i>{{ __('Solution Manager - Creative Tim Officer') }}
-                            </div>
-                            <div>
-                                <i class="ni education_hat mr-2"></i>{{ __('University of Computer Science') }}
-                            </div>
-                            <hr class="my-4" />
-                            <p>{{ __('Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.') }}</p>
-                            <a href="#">{{ __('Show more') }}</a>
                         </div>
                     </div>
                 </div>
