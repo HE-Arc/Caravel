6f8154193d3d5c9abe4906ced60b8b0f9e437134
M4n0x <M4n0x@users.noreply.github.com>
2020-11-06 15:43:36 +0100

    Set and get default group

--- app/Http/Middleware/CheckGroup.php
+++ app/Http/Middleware/CheckGroup.php
@@ -9,6 +9,8 @@ use App\Models\Group;
 
 class CheckGroup
 {
+    const SESSION_LAST_GROUP_ID = 'lastGroupId';
+
     /**
      * Handle an incoming request.
      *
@@ -19,15 +21,31 @@ class CheckGroup
     public function handle(Request $request, Closure $next)
     {
         $group = $request->route('group');
+
+        $user = auth()->user();
+
+        if (empty($group)) {
+            $group = $this->getDefaultGroup($request, $user);
+        }
         
         if (!empty($group) && !($group instanceof Group)) $group = Group::find($group);
 
-        if ($group instanceof Group) {
-            if (!$group->usersApproved->contains(auth()->user())) abort(403);
-        } elseif (empty($group)) {
-            abort(500); // this case is not possible unless this request is an attack so we abort it
+        if (empty($group) || ($group instanceof Group && !$group->usersApproved->contains($user))) {
+            abort(403);
         }
+
+        $request->session()->put(CheckGroup::SESSION_LAST_GROUP_ID, $group->id);
             
         return $next($request);
+    }
+
+    private function getDefaultGroup(Request $request, $user) {
+        $group = $request->session()->pull(CheckGroup::SESSION_LAST_GROUP_ID, '');
+
+        if (empty($group)) {
+            $group = $user->groupsAvailable()->limit(1)->pluck('id')->first();
+        }
+
+        return $group;
     }
 }
