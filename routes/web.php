19feeecffffac5fba80d907000df37c30800e29f
M4n0x <M4n0x@users.noreply.github.com>
2020-11-06 16:10:46 +0100

    fix error phpunit

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('groups/filtered/{string}', ['as' => 'groups.filtered', 'uses' => 'App\Http\Controllers\GroupController@filtered']);
 
