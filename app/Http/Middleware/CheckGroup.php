1e3be3ba970c348fca7c66c31383d9d382714482
M4n0x <M4n0x@users.noreply.github.com>
2020-11-06 12:52:20 +0100

    Fix error when group is not good

--- app/Http/Middleware/CheckGroup.php
+++ app/Http/Middleware/CheckGroup.php
@@ -22,8 +22,10 @@ class CheckGroup
         
         if (!empty($group) && !($group instanceof Group)) $group = Group::find($group);
 
-        if (!empty($group)) {
+        if ($group instanceof Group) {
             if (!$group->usersApproved->contains(auth()->user())) abort(403);
+        } elseif (empty($group)) {
+            abort(500); // this case is not possible unless this request is an attack so we abort it
         }
             
         return $next($request);
