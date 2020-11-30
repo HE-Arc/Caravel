23c505b739a19c1011fff4d8e4a929839b6feffa
Monnet Joris <joris.monnet@he-arc.ch>
2020-11-30 17:13:15 +0100

    fix import Multiple OAuth login for logout function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
 use App\Models\User;
 use Socialite;
 
