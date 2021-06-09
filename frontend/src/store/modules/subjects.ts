bef3e210c53631d43c329a4220c3c4920a4d5a02
Steve Mendes Reis <steve.mendesreis@he-arc.ch>
2021-06-09 22:57:44 +0200

    Added module subject

---
+++ frontend/src/store/modules/subjects.ts
@@ -0,0 +1,135 @@
+import store from "@/store";
+import { Subject } from "@/types/subject";
+import axios, { AxiosResponse } from "axios";
+import {
+  VuexModule,
+  Module,
+  Mutation,
+  Action,
+  getModule,
+} from "vuex-module-decorators";
+
+@Module({
+  namespaced: true,
+  dynamic: true,
+  store,
+  name: "subjects",
+  preserveState: localStorage.getItem("vuex") !== null,
+})
+class SubjectModule extends VuexModule {
+  _subjects: Subject[] = []
+  _status = "";
+
+  get subjects(): Subject[] {
+    return this._subjects;
+  }
+
+  get status(): string {
+    return this._status;
+  }
+
+  @Mutation
+  private ERROR() {
+    this._status = "error";
+  }
+
+  @Mutation
+  private REQUEST() {
+    this._status = "loading";
+  }
+
+  @Mutation
+  private LOAD_SUBJECTS(subjects: Subject[]) {
+    this._subjects = subjects;
+    this._status = "subjects_loaded";
+  }
+
+  @Mutation
+  private UPSERT_SUBJECT(subject: Subject) {
+    let index = this._subjects.findIndex(item => item.id == subject.id);
+    if (index === -1) {
+      this._subjects.push(subject);
+      this._status = "subject_added";
+    } else {
+      this._subjects[index] = subject;
+      this._status = "subject_modified";
+    }
+  }
+
+  @Mutation
+  private REMOVE_SUBJECT(subject: Subject) {
+    let index = this._subjects.findIndex(item => item.id == subject.id);
+    if (index !== -1) {
+      this._subjects.splice(index, 1);
+      this._status = "subject_delete";
+    }
+  }
+
+  @Action
+  add(groupId: string, subject: Subject): Promise<AxiosResponse> {
+    return new Promise<AxiosResponse>((resolve, reject) => {
+      axios({
+        url: process.env.VUE_APP_API_BASE_URL + `groups/${groupId}/subjects`,
+        method: 'POST',
+        data: subject,
+      })
+        .then((response) => {
+          const subject: Subject = response.data;
+          this.UPSERT_SUBJECT(subject);
+          resolve(response);
+        })
+        .catch((err) => {
+          this.ERROR();
+          reject(err);
+        });
+    });
+  }
+
+  @Action
+  update(groupId: string, subject: Subject): Promise<AxiosResponse> {
+    return new Promise<AxiosResponse>((resolve, reject) => {
+      axios({
+        url: process.env.VUE_APP_API_BASE_URL + `groups/${groupId}/subjects/${subject.id}`,
+        method: 'PATCH',
+        data: subject,
+      })
+        .then((response) => {
+          const data: Subject = response.data;
+          this.UPSERT_SUBJECT(subject);
+          resolve(response);
+        })
+        .catch((err) => {
+          this.ERROR();
+          reject(err);
+        });
+    });
+  }
+
+  @Action
+  delete(groupId: string, subject: Subject): Promise<AxiosResponse> {
+    return new Promise<AxiosResponse>((resolve, reject) => {
+      axios({
+        url: process.env.VUE_APP_API_BASE_URL + `groups/${groupId}/subjects/${subject.id}`,
+        method: 'DELETE',
+      })
+        .then((response) => {
+          this.REMOVE_SUBJECT(subject);
+          resolve(response);
+        })
+        .catch((err) => {
+          this.ERROR();
+          reject(err);
+        });
+    });
+  }
+
+  @Action
+  load(subjects: Subject[]) {
+    this.LOAD_SUBJECTS(subjects);
+  }
+
+}
+
+const instance = getModule(SubjectModule);
+
+export default instance;
---
+++ frontend/src/types/subject.ts
@@ -0,0 +1,6 @@
+export interface Subject {
+  id: string,
+  name: string,
+  color: string,
+  ects: number,
+}
