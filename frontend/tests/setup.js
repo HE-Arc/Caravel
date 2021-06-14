9f0cc2865e337b36dcf3c9215c4eb42b909555bf
Steve Mendes Reis <steve.mendesreis@he-arc.ch>
2021-06-14 21:25:36 +0200

    fix i18n test bug

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,5 +1,6 @@
 // test/setup.js
-
+import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+registerRequireContextHook();
 import Vue from "vue";
 import Vuetify from "vuetify";
 
